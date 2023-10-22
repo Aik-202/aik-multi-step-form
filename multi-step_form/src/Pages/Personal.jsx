1590da327038879804238e06d50c43b1bc75228b
Aik-202 <ikogweassurance85@gmail.com>
2023-10-22 13:13:12 +0100

    Made an adjustment to the height

--- multi-step_form/src/Pages/Personal.jsx
+++ multi-step_form/src/Pages/Personal.jsx
@@ -3,7 +3,7 @@ import { PersonalInfo, NavBar } from '../Components'
 
 export default function Personal() {
   return (
-    <section className='bg-light-blue relative h-full sm:flex sm:flex-row sm:bg-white
+    <section className='bg-light-blue relative h-screen sm:flex sm:flex-row sm:bg-white
     sm:justify-center sm:items-center sm:content-center sm:pl-5 sm:h-max sm:rounded-xl'>
         <NavBar personal={true} />
         <PersonalInfo />
