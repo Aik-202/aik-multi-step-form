7ce2f9dced6f11b1876c84928be1924e542dd3dd
Aik-202 <ikogweassurance85@gmail.com>
2023-10-24 17:54:33 +0100

    Made the addons page responsive for desktop

--- multi-step_form/src/Components/Form/AddOnsInfo.jsx
+++ multi-step_form/src/Components/Form/AddOnsInfo.jsx
@@ -5,7 +5,16 @@ import { addons } from '../../Data/demo';
 
 export default function AddOnsInfo() {
   const navigate = useNavigate();
-  const [addonActive, setAddonActive] = React.useState('Online service');
+  const [addonActive, setAddonActive] = React.useState('');
+  const [check, setCheck] = React.useState(false);
+
+  const checked = (e) => {
+    if (e.target.checked) {
+      setCheck(true)
+    } else {
+      setCheck(false)
+    }
+  }
 
   const changeAddon = (e) => {
     if (e.target.id == "Online service") {
@@ -28,28 +37,28 @@ export default function AddOnsInfo() {
   }
 
   return (
-    <section>
+    <section className='w-max lg:w-[30rem]'>
       <div className='bg-white flex flex-col space-y-5 justify-center content-center px-5 
       absolute h-max top-24 right-0 left-0 py-4 mx-5 rounded-md sm:static md:mr-10'>
          <FormHeaders personal={false} plans={false} addons={true} finished={false}/>
          {addons.map((item) => {return <div className={`flex flex-row space-x-3
          border-[1px] rounded-lg px-3 py-4 
-          ${addonActive == "Online service" && item.header == "Online service" ? 'bg-light-blue bg-opacity-[20%] border-t-marine border-purplish': 
-          addonActive == "Larger storage" && item.header == "Larger storage" ? 'bg-light-blue  bg-opacity-[20%] border-t-marine border-purplish': 
-          addonActive == "Customizable profile" && item.header == "Customizable profile" ? 'bg-light-blue  bg-opacity-[20%] border-t-marine border-purplish': 
+          ${addonActive == "Online service" && check && item.header == "Online service" ? 'bg-light-blue bg-opacity-[20%] border-t-marine border-purplish': 
+          addonActive == "Larger storage" && check && item.header == "Larger storage" ? 'bg-light-blue  bg-opacity-[20%] border-t-marine border-purplish': 
+          addonActive == "Customizable profile" && check && item.header == "Customizable profile" ? 'bg-light-blue  bg-opacity-[20%] border-t-marine border-purplish': 
           'bg-transparent border-light-gray border-t-light-gray'
           }`} onClick={changeAddon} key={item.header}>
-          <input type="checkbox" id={item.header} className='w-[7.5%]' />
+          <input type="checkbox" id={item.header} className='w-[7.5%]' onClick={checked}/>
           <div className='flex flex-col space-y-0'>
             <label htmlFor={item.header} className='text-sm text-marine font-bold tracking-wide'>
             {item.header}</label>
-            <p className='text-xs w-[11rem]'>{item.text}</p>
+            <p className='text-xs w-[11rem] xl:w-[15rem]'>{item.text}</p>
           </div>
           <p className='self-center text-sm text-purplish'>{item.month}</p>
          </div>})}
       </div>
       <div className='flex justify-between bg-white 
-      py-5 px-8 absolute bottom-0 right-0 left-0 sm:static sm:pr-10 md:pr-16 sm:rounded-xl lg:mt-10'>
+      py-5 px-8 absolute bottom-0 right-0 left-0 sm:static sm:pr-10 sm:pl-12 md:pr-16 sm:rounded-xl lg:mt-10'>
         <button className='text-cool font-medium' onClick={back}>Go Back</button>
         <button className='border-[1px] border-solid border-transparent rounded-md px-3 py-2 text-white 
       bg-marine' onClick={forward}>Next Step</button>
