b6d8214dcfc8904c51282ed1586bd4b9b78df2e3
Aik-202 <ikogweassurance85@gmail.com>
2023-10-25 10:26:47 +0100

    Made some adjustments, and added the html for the finished page

--- multi-step_form/src/Components/Form/AddOnsInfo.jsx
+++ multi-step_form/src/Components/Form/AddOnsInfo.jsx
@@ -28,7 +28,7 @@ export default function AddOnsInfo() {
   }
 
   return (
-    <section className='w-max lg:w-[30rem]'>
+    <section className='w-max sm:py-2 sm:w-[28rem] lg:py-0 lg:w-[32rem] xl:w-[30rem]'>
       <div className='bg-white flex flex-col space-y-5 justify-center content-center px-5 
       absolute h-max top-24 right-0 left-0 py-4 mx-5 rounded-md sm:static md:mr-10'>
          <FormHeaders personal={false} plans={false} addons={true} finished={false}/>
--- multi-step_form/src/Components/Form/FinishedInfo.jsx
+++ multi-step_form/src/Components/Form/FinishedInfo.jsx
@@ -13,6 +13,27 @@ export default function FinishedInfo() {
       <div className='bg-white flex flex-col space-y-5 justify-center content-center px-5 
       absolute h-max top-24 right-0 left-0 py-4 mx-5 rounded-md sm:static md:mr-10'>
         <FormHeaders personal={false} plans={false} addons={false} finished={true}/>
+        <div>
+          <div>
+            <div>
+              <h2>Arcade (Monthly)</h2>
+              <button>Change</button>
+            </div>
+            <p>$9/mo</p>
+          </div>
+          <div>
+            <h2>Online Service</h2>
+            <p>+$1/mo</p>
+          </div>
+          <div>
+            <h2>Large storage</h2>
+            <p>+$2/mo</p>
+          </div>
+        </div>
+        <div>
+          <h2>Total (per month)</h2>
+          <p>+$12/mo</p>
+        </div>
       </div>
       <div className='flex justify-between bg-white 
       py-5 px-8 absolute bottom-0 right-0 left-0 sm:static sm:pr-10 sm:pl-12 md:pr-16 sm:rounded-xl lg:mt-10'>
